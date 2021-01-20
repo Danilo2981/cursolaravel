a94fe9f190035822bf9c6bb36b58b3d76080b9dc
Danilo Vega <danilo.vega@hotmail.es>
2021-01-20 21:00:40 +0000

    restore parente user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,12 +78,7 @@ class UserController extends Controller
     public function restore($id)
     {
         $user = User::onlyTrashed()->where('id', $id)->firstOrFail();
-        $user->profile()->restore();
-
-        DB::table('user_skill')
-            ->where('user_id', $user->id)
-            ->update(array('deleted_at' => null));
-
+        
         $user->restore();
 
         return redirect()->route('users.index');
